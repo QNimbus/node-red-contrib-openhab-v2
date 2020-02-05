ba0be0333f5e6ecd1c897395c013eadfac2f76cb
Bas van Wetten <bas.van.wetten@gmail.com>
2020-02-05 20:22:55 +0100

    Save progress #2

--- static/js/node-red-openhab-v2-controller.js
+++ static/js/node-red-openhab-v2-controller.js
@@ -1,41 +1,73 @@
+/*
+
+OpenHAB nodes for IBM's Node-Red
+https://github.com/QNimbus/node-red-contrib-openhab2
+(c) 2020, Bas van Wetten <bas.van.wetten@gmail.com>
+
+MIT License
+
+Copyright (c) 2020 B. van Wetten
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+/* global RED */
+
 RED.nodes.registerType('openhab-v2-controller', {
-    category: 'config',
-    defaults: {
-        name: {
-            value: '',
-            required: true,
-        },
-        protocol: {
-            value: 'http',
-            required: true,
-        },
-        host: {
-            value: 'localhost',
-            required: true,
-        },
-        port: {
-            value: 8080,
-            validate: RED.validators.number(),
-            required: false,
-        },
-        path: {
-            value: '',
-        },
-        username: {
-            value: '',
-            required: false,
-        },
-        password: {
-            value: '',
-            required: false,
-        },
-        allowRawEvents: {
-            value: true,
-            required: true,
-        }
+  category: 'config',
+  defaults: {
+    name: {
+      value: '',
+      required: true
     },
-    paletteLabel: 'openhab-v2-controller',
-    label: function () {
-        return this.name;
+    protocol: {
+      value: 'http',
+      required: true
+    },
+    allowInsecure: {
+      value: false,
+      required: true
+    },
+    host: {
+      value: 'localhost',
+      required: true
+    },
+    port: {
+      value: 8080,
+      validate: RED.validators.number(),
+      required: true
+    },
+    username: {
+      value: '',
+      required: false
+    },
+    password: {
+      value: '',
+      required: false
     }
+  },
+  paletteLabel: 'openhab-v2-controller',
+  label: function() {
+    return this.name;
+  },
+  labelStyle: function() {
+    return this.name ? 'node_label_italic' : '';
+  }
 });
--- static/js/node-red-openhab-v2-in.js
+++ static/js/node-red-openhab-v2-in.js
@@ -1,63 +1,106 @@
+/*
+
+OpenHAB nodes for IBM's Node-Red
+https://github.com/QNimbus/node-red-contrib-openhab2
+(c) 2020, Bas van Wetten <bas.van.wetten@gmail.com>
+
+MIT License
+
+Copyright (c) 2020 B. van Wetten
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+/* eslint-env browser,jquery */
+/* global RED */
+
 RED.nodes.registerType('openhab-v2-in', {
-    category: 'OpenHAB',
-    color: '#ff6600',
-    align: 'left',
-    defaults: {
-        name: {
-            value: '',
-        },
-        controller: {
-            value: '',
-            type: 'openhab-v2-controller',
-            required: true,
-        },
-        item: {
-            value: '',
-            required: false,
-        },
-        ohCompatibleTimestamp: {
-            value: false,
-        },
-        eventTypes: {
-            value: [],
-            required: true,
-        },
-        outputAtStartup: {
-            value: true,
-            required: true,
-        },
-        storeStateInFlow: {
-            value: false,
-            required: true,
-        },
+  category: 'OpenHAB',
+  // Styling
+  icon: 'node-red-contrib-openhab-v2-color.png',
+  color: '#fff',
+  align: 'left',
+  paletteLabel: 'in',
+  label: function() {
+    return this.name || this.item || 'in';
+  },
+  labelStyle: function() {
+    return this.name || this.item ? 'node_label_italic' : '';
+  },
+  // Inputs & outputs
+  inputs: 0,
+  outputs: 1,
+  inputLabels: [],
+  outputLabels: ['StateEvent'],
+  // Default
+  defaults: {
+    name: {
+      value: ''
     },
-    inputs: 0,
-    outputs: 2,
-    inputLabels: [],
-    outputLabels: ['StateEvent', 'RawEvent'],
-    icon: 'node-red-contrib-openhab-v2.png',
-    paletteLabel: 'in',
-    label: function () {
-        return (this.name || this.item || 'in');
+    controller: {
+      value: '',
+      type: 'openhab-v2-controller',
+      required: true
     },
-    oneditprepare: function () {
-        var node = this;
-
-        getItemList(node.item, $('#node-input-controller'), '#node-input-item', true);
-        getEventTypeList(node);
-
-        // Enable/Disable OpenHAB compatible timestamp
-        $('#node-input-ohCompatibleTimestamp').change(() => {
-            if ($('#node-input-ohCompatibleTimestamp').is(':checked')) {
-                node.ohCompatibleTimestamp = true;
-            } else {
-                node.ohCompatibleTimestamp = false;
-            }
-        });
+    item: {
+      value: '',
+      required: false
     },
-    oneditsave: function () {
-        var node = this;
-
-        workaroundForEmptyItemsList(node, '#node-input-eventTypes');
+    ohCompatibleTimestamp: {
+      value: false
+    },
+    eventTypes: {
+      value: [],
+      required: true
+    },
+    outputAtStartup: {
+      value: true,
+      required: true
+    },
+    storeStateInFlow: {
+      value: false,
+      required: true
     }
+  },
+  // Dialog events
+  oneditprepare: function() {
+    const node = this;
+
+    // getItemList(node.item, $('#node-input-controller'), '#node-input-item', true);
+    // getEventTypeList(node);
+
+    // // Enable/Disable OpenHAB compatible timestamp
+    // $('#node-input-ohCompatibleTimestamp').change(() => {
+    //   if ($('#node-input-ohCompatibleTimestamp').is(':checked')) {
+    //     node.ohCompatibleTimestamp = true;
+    //   } else {
+    //     node.ohCompatibleTimestamp = false;
+    //   }
+    // });
+  },
+  oneditsave: function() {
+    const node = this;
+
+    // workaroundForEmptyItemsList(node, '#node-input-eventTypes');
+  },
+  oneditcancel: function() {},
+  oneditdelete: function() {},
+  oneditresize: function() {}
 });
