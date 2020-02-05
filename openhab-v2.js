3f6a398e8b814b7b894c7ed7e70cede7e7ac4171
Bas van Wetten <bas.van.wetten@gmail.com>
2020-02-05 20:22:55 +0100

    Save progress #2

--- openhab-v2.js
+++ openhab-v2.js
@@ -34,44 +34,62 @@ module.exports = function(RED) {
    */
 
   const path = require('path');
+  const Request = require('request');
   const EventSource = require('eventsource');
 
   /**
    * Constants
    */
 
+  const NODE_STATE = 'NODE_STATE';
   const EVENTSOURCE_STATE = 'EVENTSOURCE_STATE';
+  const NODE_STATE_TYPE = Object.freeze({ ERROR: 99 });
   const EVENTSOURCE_STATE_TYPE = Object.freeze({ CONNECTING: 0, CONNECTED: 1, DISCONNECTED: 2, ERROR: 3 });
 
   /**
    * Helper methods
    */
-
-  const updateNodeStatus = (node, newState, newStateText) => {
+  const updateNodeStatus = (node, newState, newStateType, newStateTypeText) => {
     // TODO Have node context retain it's current state message
     const currentStateText = ''; // node.context().get('currentState');
 
-    switch (newState) {
-      case EVENTSOURCE_STATE_TYPE.CONNECTED: {
-        node.status({ fill: 'green', shape: 'dot', text: newStateText !== undefined ? newStateText : 'Connected' });
-        break;
+    if (newState === EVENTSOURCE_STATE) {
+      switch (newStateType) {
+        case EVENTSOURCE_STATE_TYPE.CONNECTED: {
+          node.status({ fill: 'green', shape: 'dot', text: newStateTypeText !== undefined ? newStateTypeText : 'Connected' });
+          break;
+        }
+        case EVENTSOURCE_STATE_TYPE.CONNECTING: {
+          node.status({ fill: 'green', shape: 'ring', text: newStateTypeText !== undefined ? newStateTypeText : 'Connecting' });
+          break;
+        }
+        case EVENTSOURCE_STATE_TYPE.DISCONNECTED: {
+          node.status({ fill: 'grey', shape: 'dot', text: newStateTypeText !== undefined ? newStateTypeText : 'Disconnected' });
+          break;
+        }
+        case EVENTSOURCE_STATE_TYPE.ERROR: {
+          node.status({ fill: 'red', shape: 'dot', text: newStateTypeText !== undefined ? newStateTypeText : 'Error' });
+          break;
+        }
+        default: {
+          node.status({ fill: 'grey', shape: 'dot', text: newStateTypeText !== undefined ? newStateTypeText : currentStateText });
+          break;
+        }
       }
-      case EVENTSOURCE_STATE_TYPE.CONNECTING: {
-        node.status({ fill: 'green', shape: 'ring', text: newStateText !== undefined ? newStateText : 'Connecting' });
-        break;
+    } else if (newState === NODE_STATE) {
+      switch (newStateType) {
+        case NODE_STATE_TYPE.ERROR: {
+          node.status({ fill: 'red', shape: 'dot', text: newStateTypeText !== undefined ? newStateTypeText : 'Error' });
+          break;
+        }
+        default: {
+          node.status({ fill: 'grey', shape: 'dot', text: newStateTypeText !== undefined ? newStateTypeText : currentStateText });
+          break;
+        }
       }
-      case EVENTSOURCE_STATE_TYPE.DISCONNECTED: {
-        node.status({ fill: 'grey', shape: 'dot', text: newStateText !== undefined ? newStateText : 'Disconnected' });
-        break;
-      }
-      case EVENTSOURCE_STATE_TYPE.ERROR: {
-        node.status({ fill: 'red', shape: 'dot', text: newStateText !== undefined ? newStateText : 'Error' });
-        break;
-      }
-      default: {
-        node.status({ fill: 'grey', shape: 'dot', text: newStateText !== undefined ? newStateText : currentStateText });
-        break;
-      }
+    } else {
+      // Clear node status
+      node.status({});
     }
   };
 
@@ -80,7 +98,9 @@ module.exports = function(RED) {
    */
 
   function OpenHABNodeController(config) {
-    // Initialize node
+    /**
+     * Initialization
+     */
     const node = this;
     RED.nodes.createNode(node, config);
 
@@ -88,8 +108,15 @@ module.exports = function(RED) {
     node.retryTimer = undefined;
 
     // Create EventSourceClient
-    const EventSourceClient = function() {
-      // Utility methods
+    const EventSourceClient = function(node) {
+      /**
+       * Constants
+       */
+      const url = node.url + '/rest/events';
+
+      /**
+       * Utility methods
+       */
       this.getState = function() {
         return node._client.readyState;
       };
@@ -106,9 +133,11 @@ module.exports = function(RED) {
         return !node._client || !(node._client instanceof EventSource) || node._client.readyState === EVENTSOURCE_STATE_TYPE.DISCONNECTED;
       };
 
-      // Event handlers
+      /**
+       * Event handlers
+       */
       this.onOpen = function(event) {
-        // TODO Log succesfull connection
+        node.log(`Connected to ${url}`);
         node.emit(EVENTSOURCE_STATE, EVENTSOURCE_STATE_TYPE.CONNECTED);
       };
 
@@ -117,17 +146,22 @@ module.exports = function(RED) {
       this.onError = function(event) {
         const message = event.message || '';
         const status = event.status;
+        let retryConnection = true;
 
         let errorMessage;
 
-        if (message.includes('ENOTFOUND') || status === 404) {
-          errorMessage = `Not found - ${node.url}`;
+        if (message.toUpperCase().includes('ENOTFOUND') || status === 404) {
+          errorMessage = `Not found - ${url}`;
         } else if (status === 401 || status === 403) {
-          errorMessage = `Not authorized - ${node.url}`;
-        } else if (message.includes('ECONNREFUSED')) {
-          errorMessage = `Connection refused - ${node.url}`;
+          errorMessage = `Not authorized - ${url}`;
+          retryConnection = false;
+        } else if (message.toUpperCase().includes('ECONNREFUSED')) {
+          errorMessage = `Connection refused - ${url}`;
+        } else if (message.toUpperCase().includes('CERTIFICATE')) {
+          errorMessage = `Certificate error - ${url}`;
+          retryConnection = false;
         } else {
-          errorMessage = `Connection lost - ${node.url}`;
+          errorMessage = `Connection lost - ${url}`;
         }
 
         // If connection was interrupted or lost then 'event.message' is undefined
@@ -135,12 +169,16 @@ module.exports = function(RED) {
         if (!event.message) {
           node.emit(EVENTSOURCE_STATE, node._client.readyState);
         } else {
-          node.retryTimer = setTimeout(() => {
-            node.client.connect();
-          }, 5000);
           node._client.removeAllListeners();
           node._client.close();
           delete node._client;
+
+          // Retry to connect in 10 seconds
+          if (retryConnection) {
+            node.retryTimer = setTimeout(() => {
+              node.client.connect();
+            }, 10000);
+          }
 
           node.emit(EVENTSOURCE_STATE, EVENTSOURCE_STATE_TYPE.ERROR, errorMessage);
         }
@@ -149,66 +187,111 @@ module.exports = function(RED) {
         node.warn(errorMessage);
       };
 
-      // Methods
+      /**
+       * Main methods
+       */
       this.connect = function() {
-        // TODO Log connection attempt
         if (!node._client || !(node._client instanceof EventSource)) {
-          node._client = new EventSource(node.url, { headers: Object.assign({}, node.headers) });
+          node._client = new EventSource(url, {
+            headers: Object.assign({}, node.clientHeaders),
+            https: { rejectUnauthorized: !node.allowInsecure }
+          });
 
           node._client.on('open', this.onOpen);
           node._client.on('message', this.onMessage);
           node._client.on('error', this.onError);
         }
 
+        node.log(`Connecting to ${url}`);
         node.emit(EVENTSOURCE_STATE, EVENTSOURCE_STATE_TYPE.CONNECTING);
 
         return node._client;
       };
 
       this.disconnect = function() {
-        // TODO Log disconnect
         // If we have an EventSource instance remove all listeners and close connection
         if (node._client && node._client instanceof EventSource) {
           node._client.removeAllListeners();
           node._client.close();
+
+          node.log(`Disconnecting from ${url}`);
+          node.emit(EVENTSOURCE_STATE, EVENTSOURCE_STATE_TYPE.DISCONNECTED);
         }
 
         // If a reconnect timer is running, cancel it
         if (node.retryTimer) {
           clearTimeout(node.retryTimer);
         }
-
-        node.emit(EVENTSOURCE_STATE, EVENTSOURCE_STATE_TYPE.DISCONNECTED);
       };
+    };
+
+    // Create RequestClient
+    const RequestClient = function() {
+      /**
+       * Main methods
+       */
+      this.get = path =>
+        new Promise((resolve, reject) => {
+          const url = node.url + path;
+          const options = {
+            url,
+            followRedirect: false,
+            rejectUnauthorized: !node.allowInsecure,
+            headers: {
+              'User-Agent': 'request'
+            }
+          };
+          node.log(`GET Request ${url}`);
+          Request.get(options, (error, response, body) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(body);
+            }
+          });
+        });
     };
 
     // Load node configuration
     node.name = config.name;
     node.host = config.host;
+
     node.port = config.port;
     node.protocol = config.protocol;
-    node.url = `${node.protocol}://${node.host}:${node.port}/rest/events`;
-    node.headers = {};
+    node.allowInsecure = !!config.allowInsecure;
+    node.url = `${node.protocol}://${node.host}:${node.port}`;
+    node.clientHeaders = {};
 
     // Create Basic Auth headers if credentials are present in node config
     if (config.username && config.password) {
       const auth = Buffer.from(`${config.username}:${config.password}`).toString('base64');
-      node.headers = Object.assign(node.headers, { Authorization: `Basic ${auth}` });
+      node.clientHeaders = Object.assign(node.clientHeaders, { Authorization: `Basic ${auth}` });
     }
 
     // Linters do not like an anonymous constructor function
-    node.client = new EventSourceClient();
+    // Therefore we first create the (named) EventSourceClient constructor and
+    // then we create an instance of the EventSourceClient and assign it to node.client
+    node.client = new EventSourceClient(node);
 
-    // Node event handlers
+    node.request = new RequestClient(node);
+
+    /**
+     * Node event handlers
+     */
     node.on('close', () => {
       node.client.disconnect();
     });
 
+    /**
+     * Node main
+     */
     node.client.connect();
   }
 
   function OpenHABNodeIn(config) {
-    // Initialize node
+    /**
+     * Initialization
+     */
     const node = this;
     const controller = RED.nodes.getNode(config.controller);
     RED.nodes.createNode(node, config);
@@ -216,19 +299,31 @@ module.exports = function(RED) {
     // Load node configuration
     node.name = config.name;
 
-    // TODO Proper handling of missing controller
     if (!controller) {
       node.warn('No controller');
+      updateNodeStatus(node, NODE_STATE, NODE_STATE_TYPE.ERROR, 'No controller');
       return false;
     }
 
-    // Node event handlers
-    controller.addListener(EVENTSOURCE_STATE, updateNodeStatus.bind(null, node));
+    /**
+     * Node event handlers
+     */
+    controller.addListener(EVENTSOURCE_STATE, updateNodeStatus.bind(null, node, EVENTSOURCE_STATE));
 
     node.on('close', () => {
-      controller.removeListener(EVENTSOURCE_STATE, updateNodeStatus.bind(null, node));
+      controller.removeListener(EVENTSOURCE_STATE, updateNodeStatus.bind(null, node, EVENTSOURCE_STATE));
       node.trace('Closing node');
     });
+
+    /**
+     * Node main
+     */
+    controller.request
+      .get('/rest/items')
+      .then(res => {})
+      .catch(error => {
+        node.error(error);
+      });
   }
 
   /**
