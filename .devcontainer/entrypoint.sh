55df2803356666c84cde5fe42effabc537aed46c
B. van Wetten <bas@vanwetten.com>
2020-11-06 21:21:37 +0000

    Workflow improvements - Added vscode launch configuration for debugging - Changed NodeRED log level from 'info' to 'debug'

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Set NodeRED logging level to debug
+sed -i -r 's/level: \"\w+\"/level: "debug"/g' /data/settings.js
+
 cd /usr/src/node-red
 
 # Add 'node-red-contrib-openhab-v2' node to NodeRED palette
